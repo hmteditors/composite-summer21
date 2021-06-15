5dfe46ecc6c0c77243c5b17fd2ff4f4912bbe68a
Neel Smith <dnsmith.neel@gmail.com>
2021-06-15 06:57:37 -0400

    Automatically updated site

--- coverage.jl
+++ coverage.jl
@@ -9,13 +9,16 @@ mdlines = ["---","layout: page",
 "nav_order: 1", "---","","","# Current coverage of editing",""]
 
 using Dates
-datestamp = "Last modified: $(now())"
+t = Dates.format(now(),"U d, Y, HH:MM")
+
+datestamp = "Last modified: $t"
 push!(mdlines, datestamp)
 
 
 using CitableText
 using CitableCorpus
 using EditorsRepo
+using StatsPlots
 
 # Instantiate EditorialRepository's:
 function repolist(dirlist)
@@ -58,6 +61,63 @@ push!(mdlines,"")
 push!(mdlines,"Iliad lines: $(length(iliadlines))")
 push!(mdlines,"")
 push!(mdlines,"Scholia: $(length(schcomments))")
+push!(mdlines,"")
+push!(mdlines, "![Summary of coverage](./coverage.png)")
+
+
+#graph coverage for a single MS for books 8 - 10.
+function coverageplot(alliliad, allschol, title)
+    il8 = filter(cn -> startswith(passagecomponent(cn.urn), "8"), alliliad) |> length
+    il9 = filter(cn -> startswith(passagecomponent(cn.urn), "9"), alliliad) |> length
+    il10 = filter(cn -> startswith(passagecomponent(cn.urn), "10"), alliliad) |> length
+    iliadlines = [il8,il9,il10]
+
+    sch8 = filter(cn -> startswith(passagecomponent(cn.urn), "8"), allschol) |> length
+    sch9 = filter(cn -> startswith(passagecomponent(cn.urn), "9"), allschol) |> length
+    sch10 = filter(cn -> startswith(passagecomponent(cn.urn), "10"), allschol) |> length
+    scholia = [sch8, sch9, sch10]
+
+    plotted = groupedbar([scholia iliadlines],
+        bar_position = :stack,
+
+        xticks=(1:3, ["Book 8", "Book 9", "Book 10"]),
+        labels =  ["Scholia" "Iliad lines" ],
+        ylabel = "Number of lines", xlabel = "Book of Iliad",
+        title = title
+    )
+    plotted
+end
+
+
+# Plot coverage for all 5 MSS in books 8 - 10
+function plotall(iliad,scholia)
+
+    vbiliad = filter(cn -> contains(workcomponent(cn.urn), "msB"),  iliad)
+    vbscholia = filter(cn -> contains(workcomponent(cn.urn), "msB"),  scholia)
+    vbplot = coverageplot(vbiliad, vbscholia, "Venetus B")
+
+    vailiad = filter(cn -> contains(workcomponent(cn.urn), "msA"),  iliad)
+    vascholia = filter(cn -> contains(workcomponent(cn.urn), "msA"),  scholia)
+    vaplot = coverageplot(vailiad, vascholia, "Venetus A")
+
+
+    e3iliad = filter(cn -> contains(workcomponent(cn.urn), "e3"),  iliad)
+    e3scholia = filter(cn -> contains(workcomponent(cn.urn), "e3"),  scholia)
+    e3plot = coverageplot(e3iliad, e3scholia, "Upsilon 1.1")
+
+
+    tiliad = filter(cn -> contains(workcomponent(cn.urn), "burney86"),  iliad)
+    tscholia = filter(cn -> contains(workcomponent(cn.urn), "burney86"),  scholia)
+    tplot = coverageplot(tiliad, tscholia, "Burney 86")
+
+
+    l = @layout [a b; c d]
+
+    plot(vaplot, e3plot, vbplot, tplot,  layout = l)
+end
+
+coverplot = plotall(iliadlines, schcomments)
+savefig(coverplot, "docs/coverage/coverage.png")
 
 delimited = cex(CitableTextCorpus(allnodes))
 cexfile = "data/s21corpus.cex"
--- docs/coverage/index.md
+++ docs/coverage/index.md
@@ -7,10 +7,12 @@ nav_order: 1
 
 # Current coverage of editing
 
-Last modified: June 15, 2021, 05:51
+Last modified: June 15, 2021, 06:56
 
 Total citable nodes: 8029
 
 Iliad lines: 3274
 
 Scholia: 1585
+
+![Summary of coverage](./coverage.png)
