d769f4e07330bab362d7a00b35318046591929e8
Neel Smith <dnsmith.neel@gmail.com>
2021-06-17 09:26:17 -0400

    Add placeholder files

---
+++ scripts/freqs.pl
@@ -0,0 +1,3 @@
+using CSV, DataFrames
+
+f = pwd()
---
+++ simplenbs/zipfer.jl
@@ -0,0 +1,23 @@
+### A Pluto.jl notebook ###
+# v0.15.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4b4ab934-cf6f-11eb-0ce5-5bfeda45ec58
+md"Zipfer for HMT"
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─4b4ab934-cf6f-11eb-0ce5-5bfeda45ec58
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
