709e6ad93e8111f0b1ce9c000fdfda8c884b6593
Neel Smith <dnsmith.neel@gmail.com>
2021-07-02 09:35:49 -0400

    Correctly select by work id

--- scripts/tfidf.jl
+++ scripts/tfidf.jl
@@ -11,4 +11,16 @@ ms = "msB"
 url = "https://raw.githubusercontent.com/hmteditors/composite-summer21/main/data/archive-normed.cex" 
 c = CitableCorpus.fromurl(CitableTextCorpus, url, "|")
 comments = filter(cn -> endswith(cn.urn.urn, "comment"), c.corpus)
+nonempty = filter(cn -> ! isempty(cn.text), comments)
 
+function mscomments(ms)
+        msnodes = ms == "all" ? nonempty : filter(cn -> occursin(string(ms,"."), cn.urn.urn), nonempty) 
+        msnodes
+end
+
+ms = "msA"
+msnodes = mscomments(ms)
+println("For ", ms, ", ", length(msnodes), " out of ", length(nonempty), " scholia.")
+
+msAnodes = mscomments("msA")
+msAurns = map(cn -> workcomponent(cn.urn), msAnodes) |> unique
