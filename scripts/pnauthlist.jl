c952f0623900f6c11d8112a232caef6e7da4236d
Neel Smith <dnsmith.neel@gmail.com>
2021-07-08 06:38:37 -0400

    Add function to check value against authlist

--- scripts/pnauthlist.jl
+++ scripts/pnauthlist.jl
@@ -5,10 +5,18 @@ using HTTP
 authurl  = "https://raw.githubusercontent.com/homermultitext/hmt-authlists/master/data/hmtnames.cex"
 authdf = CSV.File(HTTP.get(authurl).body; delim = "#", header = 2) |> DataFrame
 
-u = "urn:cite2:hmt:pers.v1:pers1"
-matched = filter( r -> r.urn == u, authdf)
 
-matched[1,:label]
-function labelforurn(u)
-	
+
+function labelforurn(u, authdf)
+    matched = filter( r -> r.urn == u, authdf)
+    if nrow(matched) > 1
+        @warn "Multiple results for $u !"
+        nothing
+    elseif nrow(matched) == 0
+        @warn "No matches for $u"
+    else
+        matched[1,:label]
+	end
 end
+
+labelforurn("urn:cite2:hmt:pers.v1:pers1002", authdf)
