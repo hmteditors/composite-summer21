985ee310c5bbdf13179607720a561fd78ac06d94
Neel Smith <dnsmith.neel@gmail.com>
2021-07-08 08:07:12 -0400

    Tidier script for rerunning

--- scripts/archive.jl
+++ scripts/archive.jl
@@ -5,33 +5,7 @@ using CitableText
 using CitableCorpus
 using EditorsRepo
 
-
-archiveroot = string(pwd() |> dirname, "/hmt-archive/archive")
-repo = repository(archiveroot; dse="dse-data", config="textconfigs", editions="tei-editions")
-
-
-#= Create a citable corpus of archival text in a repo
-function archivalcorpus(r::EditingRepository)
-    citesdf = citation_df(repo)
-    urns = citesdf[:, :urn]
-
-    corpora = []
-    for u in urns
-        # 1. Read the source text (here, XML)
-        src = textsourceforurn(r, u)
-        if isnothing(src)
-            # skip it
-        else
-            # 2. get the EditionBuilder for the urn
-            reader = ohco2forurn(citesdf, u)
-            # 3. create citable corpus of the archival version
-            push!(corpora, reader(src, u))
-        end
-    end
-    CitableCorpus.composite_array(corpora)
-end
-=#
-archivaltexts = archivalcorpus(repo)
+# Write CEX version of all texts in corpus c
 function writearchivalcex(c::CitableTextCorpus, f = "data/archive-xml.cex")
     nonempty = filter(cn -> ! isempty(cn.text), c.corpus) |> CitableTextCorpus
     rawcex = cex(nonempty)
@@ -46,4 +20,10 @@ function writearchivalcex(c::CitableTextCorpus, f = "data/archive-xml.cex")
     end
 end
 
+
+archiveroot = string(pwd() |> dirname, "/hmt-archive/archive")
+repo = repository(archiveroot; dse="dse-data", config="textconfigs", editions="tei-editions")
+archivaltexts = archivalcorpus(repo)
 writearchivalcex(archivaltexts)
+
+#
