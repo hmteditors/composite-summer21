ea40f4c36704df49b397d470268e54fd0a89e253
Neel Smith <dnsmith.neel@gmail.com>
2021-07-29 08:26:43 -0400

    Verify all nodes, not just scholia

--- buildscripts/verify.jl
+++ buildscripts/verify.jl
@@ -16,17 +16,21 @@ archival = "data/archive-xml.cex"
 normalized = "data/archive-normed.cex"
 
 archivalcorp = CitableCorpus.fromfile(CitableTextCorpus, archival)
-scholianodes = filter(cn -> contains(cn.urn.urn,"tlg5026"), archivalcorp.corpus)
-archivalscholia = filter(cn -> ! endswith(cn.urn.urn, "ref"), scholianodes) |> CitableTextCorpus
+#scholianodes = filter(cn -> contains(cn.urn.urn,"tlg5026"), archivalcorp.corpus)
+#archivalscholia = filter(cn -> ! endswith(cn.urn.urn, "ref"), scholianodes) |> CitableTextCorpus
 
 normalizedcorp = CitableCorpus.fromfile(CitableTextCorpus, normalized)
 
-if length(archivalscholia.corpus) != length(normalizedcorp.corpus)
+
+if length(archivalcorp.corpus) != length(normalizedcorp.corpus)
     @error("Corpora of different lengths!")
-    @info("Archival: $(length(archivalscholia.corpus)), normalized: $(length(normalizedcorp.corpus))")
-    archivedurns = map(cn -> dropversion(cn.urn).urn, archivalscholia.corpus)
+    @info("Archival: $(length(archivalcorp.corpus)) ")
+    @info("Normalized: $(length(normalizedcorp.corpus))")
+    archivedurns = map(cn -> dropversion(cn.urn).urn, archivalcorp.corpus)
     normalizedurns = map(cn -> dropversion(cn.urn).urn, normalizedcorp.corpus)
-    diffs = setdiff(Set(archivedurns), Set(normalizedurns))
+    setA = Set(archivedurns)
+    setN = Set(normalizedurns)
+    diffs = setdiff(setA, setN )
     
     open("urndiffs.txt", "w") do io
         write(io, join(diffs,"\n"))
@@ -41,13 +45,16 @@ end
 #archivalwks = map(cn -> workcomponent(cn.urn), archivalscholia.corpus) |> unique
 #normalizedwks = map(cn -> workcomponent(cn.urn), normalizedcorp.corpus) |> unique
 
+#=
+archivedurns = map(cn -> dropversion(cn.urn).urn, archivalcorp.corpus)
+normalizedurns = map(cn -> dropversion(cn.urn).urn, normalizedcorp.corpus)
 
-#archivedsorted = sort(archivedurn)
-#normedsorted = sort(normalizedurn)
+archivedsorted = sort(archivedurns)
+normedsorted = sort(normalizedurns)
 
-
-
-
+println(join(archivedsorted,"\n")) 
+println(join(normedsorted,"\n")) 
+=#
 
 #tmedition = "data/topicmodelingedition.cex"
 #tmsrcedition = "data/tompicmodelingsource.cex"
